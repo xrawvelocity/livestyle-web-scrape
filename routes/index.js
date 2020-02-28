8a5e7932d53332b5f858ff577e00e9eaeaaa2ea1
Victor Fernandez <vmfs99@gmail.com>
2020-02-27 19:20:39 -0500

    s

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,7 @@ const router = express.Router()
 
 router.get('/scrapeAmazon', (req, res, next)=>{
     console.log('cheetah?!!')
-    axios.get(`https://www.amazon.com/s?k=${req.query.q}`,
+    axios.get(`https://www.amazon.com/s?k=${req.query.q}&random=${Math.random()}`,
     {
       headers: {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'
